a983874990f3b161310342bcfeec6677f4fd9d18
BullFei <tian18840876830@gmail.com>
2020-11-12 17:15:51 +0800

    搜索和搜索结果页面

--- src/main.js
+++ src/main.js
@@ -12,14 +12,14 @@ Vue.config.productionTip = false
  * 第一个参数  是过滤器的名字
  * 第二个参数  回调函数，必须有返回值
 */
-// Vue.filter('formatYi', (value) => {
-//   var Yi = Math.pow(10, 8)
-//   if (value > Yi) {
-//     return `${(value / Yi).toFixed(2)}亿`
-//   } else {
-//     return `${(value / Math.pow(10, 4)).toFixed(2)}万`
-//   }
-// })
+Vue.filter('formatYi', (value) => {
+  var Yi = Math.pow(10, 8)
+  if (value > Yi) {
+    return `${(value / Yi).toFixed(2)}亿`
+  } else {
+    return `${(value / Math.pow(10, 4)).toFixed(2)}万`
+  }
+})
 
 new Vue({
   router,
